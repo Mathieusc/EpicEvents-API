2884c5b3dea2c5cc3b15b53495486aefb4d848fb
Mathieusc <mathieu.schweitzer@hotmail.fr>
2022-05-19 15:40:51 +0200

    added all models views and urls

--- crm_api/serializers.py
+++ crm_api/serializers.py
@@ -25,3 +25,51 @@ class ClientDetailSerializer(ModelSerializer):
             "date_updated",
             "sales",
         ]
+
+
+class ContractListSerializer(ModelSerializer):
+    class Meta:
+        model = Contract
+        fields = ["id", "client", "amount", "date_created"]
+
+
+class ContractDetailSerializer(ModelSerializer):
+    client_email = serializers.ReadOnlyField(source="client.email")
+
+    class Meta:
+        model = Contract
+        fields = [
+            "id",
+            "client",
+            "client_email",
+            "date_created",
+            "date_updated",
+            "status",
+            "amount",
+            "payment_due",
+        ]
+
+
+class EventListSerializer(ModelSerializer):
+    client_email = serializers.ReadOnlyField(source="client.email")
+
+    class Meta:
+        model = Event
+        fields = ["id", "contract", "client_email", "event_dates"]
+
+
+class EventDetailSerializer(ModelSerializer):
+    client_company_name = serializers.ReadOnlyField(source="client.company_name")
+
+    class Meta:
+        model = Event
+        fields = [
+            "id",
+            "client_company_name",
+            "status",
+            "date_created",
+            "date_updated",
+            "attendees",
+            "event_dates",
+            "notes",
+        ]
--- crm_api/urls.py
+++ crm_api/urls.py
@@ -2,12 +2,20 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from crm_api.views import ClientViewSet
+from crm_api.views import ClientViewSet, ContractViewSet, EventViewSet
 
 client_router = routers.SimpleRouter()
 client_router.register("clients", ClientViewSet, basename="client")
 
+contract_router = routers.SimpleRouter()
+contract_router.register("contracts", ContractViewSet, basename="contract")
+
+event_router = routers.SimpleRouter()
+event_router.register("events", EventViewSet, basename="event")
+
 urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
     path("", include(client_router.urls)),
+    path("", include(contract_router.urls)),
+    path("", include(event_router.urls)),
 ]
